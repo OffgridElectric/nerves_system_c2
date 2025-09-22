8c71b92f3e808aa463fd26f8d6a2f3449a253d54
Venkat Subbiah <venkat.subbiah@zolaelectric.com>
2025-09-22 15:24:36 -0700

    Adding helper script for build.

--- docker/artifact_build_helper.sh
+++ docker/artifact_build_helper.sh
@@ -3,3 +3,12 @@ set_artifact_env()
     export MIX_TARGET=c2
     export MIX_ENV=prod
 }
+
+build()
+{
+    WS=/work/nerves_system_c2
+    cd $WS ; mix deps.get
+    cd $WS/test_c2 ; mix deps.get
+    mix firmware
+    cd $WS ; mix nerves.artifact
+}
