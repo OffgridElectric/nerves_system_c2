fc93e789af30eb15616e92c9de424d630deb4b88
vmanchala-dev <vijay.manchala@zolaelectric.com>
2025-10-05 22:20:18 +0530

    Modify c2_docker_shell.sh to execute passed commands inside Docker container

--- docker/c2_docker_shell.sh
+++ docker/c2_docker_shell.sh
@@ -1,5 +1,7 @@
-docker run --rm -it \
+#!/bin/bash
+docker run --rm \
   --network host \
   -v $(pwd):/work/nerves_system_c2 \
   -v ~/.ssh:/home/dev/.ssh:ro \
-  c2_dev
+  c2_dev \
+  bash -c "$1"
