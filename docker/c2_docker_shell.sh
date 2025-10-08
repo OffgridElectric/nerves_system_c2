244e323504668dfae22fb25991c4668016bd9d9e
vmanchala-dev <vijay.manchala@zolaelectric.com>
2025-10-08 14:03:36 +0530

    add e flags propagate those environment variables into the container

--- docker/c2_docker_shell.sh
+++ docker/c2_docker_shell.sh
@@ -5,6 +5,7 @@ docker run --rm \
   -v $(pwd):/work/nerves_system_c2 \
   -v ~/.ssh:/home/dev/.ssh:ro \
   -u $(id -u):$(id -g) \
+  -e CI_GITHUB_USER \
+  -e CI_GITHUB_TOKEN \
   c2_dev \
   bash -lc "$1"
-
